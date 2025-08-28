d2ba3d5aa38a148f5facb25130de12c38f6967ef
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-08-28 00:47:06 -0700

    Add starting point

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import gifos
+import shutil
+from gifos.utils.load_config import gifos_settings
+
+# github_stats = gifos.utils.fetch_github_stats(
+#     user_name="Ethorbit"
+# )
+
+user = "Ethorbit"
+host = "github.com"
+prompt = f"┌──(\033[32m{user}\033[37m@\033[31m{host}\033[37m)-[~]\n└─$"
+
+t = gifos.Terminal(width=500, height=500, xpad=5, ypad=5)
+t.set_font("/usr/share/fonts/truetype/FiraCodeNerdFont-Regular.ttf", 11)
+t.set_prompt(prompt)
+t.gen_prompt(1)
+t.gen_text("", row_num=1)
+t.gen_typing_text(" whoami", row_num=2, speed=2, contin=True)
+t.gen_prompt(t.curr_row)
+t.set_txt_color("green")
+t.gen_text(user, row_num=3)
+t.gen_text("", row_num=4)
+t.set_txt_color("white")
+t.gen_prompt(5)
+t.clone_frame(30)
+t.gen_gif()
+
+# gif -> Docker volume
+shutil.copy(
+    (
+        gifos_settings.get("files", {}).get("output_gif_name") or "output"
+    ) + ".gif",
+    "/output"
+)
